8104b60c1acdee0ee8c410ff38dd8dbbf7a132d2
yfove <curtis.ch.chat@gmail.com>
2021-02-04 15:06:17 -0700

    Styling Faq Section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,22 +5,25 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+// styles
+import { About, Description, Image } from "../styles";
+import styled from "styled-components";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
           <div className="card">
             <div className="icon">
               <img src={teamwork} alt="" />
@@ -42,13 +45,41 @@ const ServicesSection = () => {
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        </div>
-      </div>
-      <div className="image">
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
