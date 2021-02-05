aaf84154406d7897248079c460e481bcf8421b3e
yfove <curtis.ch.chat@gmail.com>
2021-02-05 00:43:18 -0700

    React Router and Our Work

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
+// switch component
+// stops as soon as it finds the first match
+// exact
+//
 
 export default App;
