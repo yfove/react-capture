460a10801fdc15bc6036372b200bc7f776ad3772
yfove <curtis.ch.chat@gmail.com>
2021-02-05 21:28:26 -0700

    Framer Motion

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "reaact-router-dom";
+import { Link } from "react-router-dom";
 //Images
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
@@ -12,21 +12,21 @@ const OurWork = () => {
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-athlete">
           <img src={athlete} alt="athlete" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/good-times">
           <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
