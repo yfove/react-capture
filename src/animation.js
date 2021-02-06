18a169f1ced2b0eda3534f56b7954ec678f23048
yfove <curtis.ch.chat@gmail.com>
2021-02-06 16:02:47 -0700

    Our Work Animation

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,6 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
     transiton: {
       duration: 0.5,
     },
@@ -47,4 +46,26 @@ export const photoAnim = {
       duration: 0.75,
     },
   },
+};
+
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
